62bbe8b3bf228ccfcd3449ce86dd54abbc99e615
Aiden Bennett <bennettaiden563@gmail.com>
2026-03-10 15:50:15 -0400

    created header file for my detector. values still needed.

---
+++ sim_02/include_02/detectors/DetectiveXHPGe.hh
@@ -0,0 +1,63 @@
+#ifndef DetectiveXHPGe_hh
+#define DetectiveXHPGe_hh
+
+#include "G4VUserDetectorConstruction.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4PhysicalConstants.hh"
+#include "G4VisAttributes.hh"
+#include "G4AssemblyVolume.hh"
+#include "G4Tubs.hh"
+#include "materials.hh"
+#include "G4RotationMatrix.hh"
+#include "G4ThreeVector.hh"
+#include "G4AssemblyVolume.hh"
+#include "G4Transform3D.hh"
+#include "G4VSolid.hh"
+#include "G4SubtractionSolid.hh"
+#include "G4Box.hh"
+
+class DetectiveXHPGe {
+    public:
+/**
+         * @brief Creates a new Cylinder Detector (NaI) object
+         * 
+         * @param name name of the sensitive volume
+         * @param radius radius of detector
+         * @param length the full length of the detector
+         * @param motherVolume the mother logical volume
+         * @param copyNumber copy number for sensitive volume
+         * @param orientationRot detector rotation relative to MOTHER volume
+         * @param orientationTrans detector tranlsation relative to MOTHER volume
+         */
+
+        DetectiveXHPGe(G4String name,
+                        G4LogicalVolume *motherVolume,
+                        G4int copyNumber,
+                        G4RotationMatrix* orientationRot,
+                        G4ThreeVector orientationTrans);
+        
+        ~DetectiveXHPGe();
+
+        /**
+         * @brief Returns the logical volume of the sensitive volume
+         * @return *G4LogicalVolume
+         */
+        G4LogicalVolume *GetSDLogicalVolume();
+
+                /**
+         * @brief Returns a G4ThreeVector containing the gaussian energy broadening parameters
+         * These values are used within the MySensitiveDetector class for creating a spectrum based on measured values.
+         * 
+         * @return G4ThreeVector
+         */
+        G4ThreeVector GetGEBParameters();
+
+    private:
+        /**
+         * @todo put in variables (radius, height, etc.) here
+         */
+};
+
+#endif
---
+++ sim_02/src_02/detectors/DetectiveXHPGe.cc
@@ -0,0 +1,1 @@
+#include "DetectiveXHPGe.hh"
